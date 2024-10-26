a41af7f38a1d4387a97f5c0963ffb4a73012a346
João Gabrieel <joaodevlopes@gmail.com>
2024-10-26 18:27:15 -0300

    Projeto Concluído
    Feito

---
+++ javas.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.lado-direito', {duration:1400});
+sr.reveal('.titulo',{
+    rotate:{x: 0, y:80, z:0},
+    duration:2000
+});/*parte da animação de transição*/
+
